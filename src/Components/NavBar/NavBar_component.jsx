2d75eb91ac0b651f46f9555f2eb6d0e6f78d0eed
Eca <your email>
2023-05-19 16:30:39 +0200

    aggiunta animazione navBar e implementatà la responsività per laptop layout, da addiungere apertura e chiusura del menu quando cliccato e le animazioni per lo scroll

--- src/Components/NavBar/NavBar_component.jsx
+++ src/Components/NavBar/NavBar_component.jsx
@@ -1,13 +1,33 @@
 import { ReactComponent as LogoSambuco } from "../../Images/SVG/LogoSambuco.svg";
 import { ReactComponent as BurgerMenu } from "../../Images/SVG/BurgerMenu.svg";
 
+import { useState, useEffect } from "react";
+
 import "./NavBar_style.css";
 
 const NavBar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div id="NavBar">
-      <LogoSambuco className="logoSambuco" />
-      <BurgerMenu className="burgerMenu" />
+    <div id="NavBar" className={scrolled ? "scrolled" : ""}>
+      <LogoSambuco className={`logoSambuco ${scrolled ? "scrolled" : ""}`} />
+      <BurgerMenu className={`burgerMenu ${scrolled ? "scrolled" : ""}`} />
     </div>
   );
 };
