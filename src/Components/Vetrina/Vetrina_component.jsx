d711b881fa734c7d14b34d4b771757a2e459a627
Eca <your email>
2023-06-03 19:57:12 +0200

    fatto layout componente vetrina generale

--- src/Components/Vetrina/Vetrina_component.jsx
+++ src/Components/Vetrina/Vetrina_component.jsx
@@ -1,10 +1,62 @@
+import NavBar from "../NavBar/NavBar_component";
+
+import vetrinaAnelli from "../../Images/ImmaginiVetrine/vetrinaAnelli.png";
+import vetrinaBracciali from "../../Images/ImmaginiVetrine/vetrinaBracciali.png";
+import vetrinaCollane from "../../Images/ImmaginiVetrine/vetrinaCollane.png";
+import vetrinaOrecchini from "../../Images/ImmaginiVetrine/vetrinaOrecchini.png";
+import vetrinaVestiti from "../../Images/ImmaginiVetrine/vetrinaVestiti.png";
+import vetrinaCorsetti from "../../Images/ImmaginiVetrine/vetrinaCorsetti.png";
+
+import Button from "../Buttons/button_component";
+
 import "./vetrina_style.css";
 
-const Vetrina = () => {
+const vetrina = {
+  anelli: vetrinaAnelli,
+  bracciali: vetrinaBracciali,
+  collane: vetrinaCollane,
+  orecchini: vetrinaOrecchini,
+  vestiti: vetrinaVestiti,
+  corsetti: vetrinaCorsetti,
+};
+
+const Vetrina = ({ immagineVetrina, categoriaProdotto, info }) => {
+  const backgroundVetrina = vetrina[immagineVetrina];
+
+  const immagineSezioneStyle = {
+    backgroundImage: `url(${backgroundVetrina})`,
+  };
+
   return (
-    <h1>
-      Eca il muratore sta ancora lavorando all'implementazione di questa pagina
-    </h1>
+    <div id="vetrinaContainer">
+      <NavBar />
+      <section className="headImageVetrina" style={immagineSezioneStyle}>
+        <h1>VETRINA {categoriaProdotto}</h1>
+      </section>
+
+      <section className="linkProdotti">
+        <p>LE MIE ALTRE VETRINE:</p>
+        <div className="pulsanti">
+          <Button text={"Anelli"} iconName={"anello"} />
+          <Button text={"Bracciali"} iconName={"bracciale"} />
+          <Button text={"Collane"} iconName={"collana"} />
+          <Button text={"Orecchini"} iconName={"orecchini"} />
+          <Button text={"Vestiti"} iconName={"vestito"} />
+          <Button text={"Corsetti"} iconName={"corsetto"} />
+        </div>
+      </section>
+
+      <section className="shopProduct">
+        <h1>prodotto 1</h1>
+        <h1>prodotto 2</h1>
+        <h1>prodotto 3</h1>
+        <h1>prodotto 4</h1>
+        <h1>prodotto 5</h1>
+        <h1>prodotto 6</h1>
+      </section>
+
+      <p className="shopInfo">{info}</p>
+    </div>
   );
 };
 
--- src/Components/VetrinaAnelli/VetrinaAnelli_component.jsx
+++ src/Components/VetrinaAnelli/VetrinaAnelli_component.jsx
@@ -3,7 +3,13 @@ import "./vetrinaAnelli_style.css";
 import Vetrina from "../Vetrina/Vetrina_component";
 
 const Anelli = () => {
-  return <Vetrina />;
+  return (
+    <Vetrina
+      immagineVetrina={"anelli"}
+      categoriaProdotto={"ANELLI"}
+      info={"**questa è una prova delle info"}
+    />
+  );
 };
 
 export default Anelli;
